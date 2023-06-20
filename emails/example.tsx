c7d2e132908846371cbc399901698b8148a33728
Rodrigo Fernandes <me@rodrigofernand.es>
2023-06-20 14:57:51 -0300

    changes

---
+++ emails/example.tsx
@@ -0,0 +1,17 @@
+import { Button,Html } from '@react-email/components';
+import * as React from 'react';
+
+export default function Email() {
+  return (
+    <Html>
+      <Button
+        pX={20}
+        pY={12}
+        href="https://example.com"
+        style={{ background: '#000', color: '#fff' }}
+      >
+        Click me
+      </Button>
+    </Html>
+  );
+}
